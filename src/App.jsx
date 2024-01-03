54f514b73c1aead4bc47005c2fae6c035beccf34
emirfurkanbilici <emirfurkanbilici@hotmail.com>
2024-01-03 14:38:09 +0300

    api instance
    Co-authored-by: Denesene Dev <denesene-dev@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -8,63 +8,85 @@ import axios from "axios";
 
 const PlotlyRenderers = createPlotlyRenderers(Plot);
 
+const api = axios.create({
+  baseURL: "https://dene-sene.net/api",
+});
+
 export default function App() {
   const [state, setState] = useState({});
+  const [isDataLoading, setIsDataLoading] = useState(false);
   const [pivotData, setPivotData] = useState([]);
 
-  const getGraphData = async () => {
-    const { data } = await axios.get(`https://dene-sene.net/api/GetProGraph`);
-    console.log(data);
-    const formattedData = data.map((item) => {
-      const orderedValues = [
-        item.MaritalStatus,
-        item.HasChildren,
-        item.EducationDegreeName,
-        item.JobStatus,
-        item.MonthlyIncomeDesc,
-        item.HasPet,
-        item.HasSocialMediaAccount,
-        item.HouseStatus,
-        item.Sex === "E" ? "Erkek" : "Kadın",
-        item.BirthDate,
-        item.CityId,
-        item.LanguageId,
+  const getGraphData = async (controller) => {
+    try {
+      setIsDataLoading(true);
+      const { data } = await api.get("/GetProGraph", {
+        signal: controller.signal,
+      });
+      console.log(data);
+      const formattedData = data.map((item) => {
+        const orderedValues = [
+          item.MaritalStatus,
+          item.HasChildren,
+          item.EducationDegreeName,
+          item.JobStatus,
+          item.MonthlyIncomeDesc,
+          item.HasPet,
+          item.HasSocialMediaAccount,
+          item.HouseStatus,
+          item.Sex === "E" ? "Erkek" : "Kadın",
+          item.BirthDate,
+          item.CityId,
+          item.LanguageId,
+        ];
+        return orderedValues;
+      });
+
+      const headers = [
+        "Medeni Hal",
+        "Çocuk",
+        "Eğitim Seviyesi",
+        "Mesleki Durum",
+        "Aylık Gelir",
+        "Evcil Hayvan",
+        "Sosyal Medya",
+        "Ev Durumu",
+        "Cinsiyet",
+        "Yaş",
+        "Şehir",
+        "Dil",
       ];
-      return orderedValues;
-    });
 
-    const headers = [
-      "Medeni Hal",
-      "Çocuk",
-      "Eğitim Seviyesi",
-      "Mesleki Durum",
-      "Aylık Gelir",
-      "Evcil Hayvan",
-      "Sosyal Medya",
-      "Ev Durumu",
-      "Cinsiyet",
-      "Yaş",
-      "Şehir",
-      "Dil",
-    ];
-
-    const formattedOutput = [headers, ...formattedData];
-    console.log(formattedOutput);
-    setPivotData(formattedOutput);
+      const formattedOutput = [headers, ...formattedData];
+      console.log(formattedOutput);
+      setPivotData(formattedOutput);
+    } catch (e) {
+      if (!axios.isCancel(e)) alert("Bir sorun oluştu!");
+    } finally {
+      setIsDataLoading(false);
+    }
   };
 
   useEffect(() => {
-    getGraphData();
+    const controller = new AbortController();
+    getGraphData(controller);
+    return () => {
+      controller.abort();
+    };
   }, []);
 
   return (
     <div id="wrapper">
-      <PivotTableUI
-        data={pivotData}
-        onChange={(s) => setState(s)}
-        renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
-        {...state}
-      />
+      {isDataLoading ? (
+        "Veri getiriliyor.."
+      ) : (
+        <PivotTableUI
+          data={pivotData}
+          onChange={(s) => setState(s)}
+          renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+          {...state}
+        />
+      )}
     </div>
   );
 }
